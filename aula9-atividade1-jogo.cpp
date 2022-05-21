5ea3cba4826713fb00e5fe92ec9350125ded064f
Dominique Fonseca <dominiquenf@gmail.com>
2022-05-21 11:51:28 -0300

    Add files via upload

---
+++ aula9-atividade1-jogo.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include <locale>
+using namespace std;
+
+int
+main ()
+{
+  setlocale (LC_ALL, "ptb");
+  int num, ind, tentativas = 0, numeros[10];
+  bool achou = false;
+
+  cout << "### PLAYER 1 ###" << endl;
+  for (ind = 0; ind <= 9; ind++)
+    {
+      cout << "Digite o " << ind + 1 << "º número: ";
+      cin >> numeros[ind];
+    }
+
+  cout << "### PLAYER 2 ###" << endl;
+  cout << "Digite um número :";
+  cin >> num;
+  tentativas=1;
+  ind=0;
+
+  while (achou == false and tentativas < 3)
+    {
+      while (achou == false and ind <= 9)
+	{
+	  if (numeros[ind] == num)
+	    {
+	      achou = true;
+	      ind++;
+	    }
+	  else
+	    {
+	      tentativas++;
+	      cout << "Número não encontrado" << endl;
+	      cout << "Tentativa nº: " << tentativas << ": " ;
+	      cin >> num;
+	      ind=0;
+	    }
+	    if (achou == true)
+	    {
+	      cout << "Parabéns, você conseguiu depois de " << tentativas << " tentativas." << endl;
+	    }
+	  else
+	    {
+	      cout << "Que pena, você não conseguiu." << endl;
+	    }
+	 	}
+	 	 
+    }
+
+
+
+
+  return 0;
+}
